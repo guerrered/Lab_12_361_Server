523b3d47996f86c208d74eca484380b4ea074306
guerrered <guerrered8@gmail.com>
2017-04-19 14:31:22 -0500

    table methods

--- src/Table.java
+++ src/Table.java
@@ -1,4 +1,32 @@
+import java.util.HashMap;
+import java.util.List;
 
 public class Table {
+	List<Player> table;
+	HashMap<Integer, Player> map;
+	
+	public Table(){
+		
+	}
+	
+	/**
+	 * 
+	 * @param s takes string s which is a JSON string
+	 * that will contain the players
+	 */
+	public void refreshTable(String s){
+		map.clear();
+		
+	}
+	
+	
+	/**
+	 * makes the list
+	 */
+	public void getListAsHTML(){
+		
+	}
+	
+	
 	
 }
