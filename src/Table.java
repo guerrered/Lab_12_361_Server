22214a55b48e84828abbbe14ff597d62bfc3f3ce
guerrered <guerrered8@gmail.com>
2017-04-19 14:44:19 -0500

    player

--- src/Table.java
+++ src/Table.java
@@ -1,12 +1,16 @@
 import java.util.HashMap;
 import java.util.List;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class Table {
 	List<Player> table;
 	HashMap<Integer, Player> map;
+	Gson g;
 	
 	public Table(){
-		
+		g = new Gson();
+		map = new HashMap<>();
 	}
 	
 	/**
@@ -15,10 +19,18 @@ public class Table {
 	 * that will contain the players
 	 */
 	public void refreshTable(String s){
+		
 		map.clear();
 		
 	}
 	
+	/**
+	 * 
+	 * @param p if p's bib number is new it is added to the map
+	 */
+	public void addToTable(Player p){
+		
+	}
 	
 	/**
 	 * makes the list
