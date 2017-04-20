bac6e2af44dc4ba39925fa98e2763ef87e02bbbe
Unknown <guerrered8@gmail.com>
2017-04-20 18:34:08 -0500

    table Ops
    Added table operations

--- src/Table.java
+++ src/Table.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import com.google.gson.Gson;
@@ -5,7 +7,7 @@ import com.google.gson.reflect.TypeToken;
 
 public class Table {
 	List<Player> table;
-	HashMap<Integer, Player> map;
+	static HashMap<Integer, Player> map;
 	Gson g;
 	
 	public Table(){
@@ -16,29 +18,86 @@ public class Table {
 	/**
 	 * 
 	 * @param s takes string s which is a JSON string
-	 * that will contain the players
+	 * that will contain the extra player information
+	 * This info will then update whatever currently exists in 
+	 * the map
 	 */
-	public void refreshTable(String s){
-		
-		map.clear();
-		
+	public void refreshTable(String json){
+		List<Player> tempList = g.fromJson(json,new TypeToken<List<Player>>(){}.getType());
+		for(Player p: tempList){
+			int key = Integer.parseInt(p.bibNumber);
+			if(map.containsKey(key)){
+				Player toMod = map.get(key);
+				toMod.time = p.time;//update the time value
+			}
+			else{
+				map.put(key, p);//adds new player to list no name only time and bibNum
+			}
+		}
 	}
 	
 	/**
 	 * 
-	 * @param p if p's bib number is new it is added to the map
+	 * @param P Player that will be added to the map
+	 * if a previous player exists with the same bib number they
+	 * will be replaced by p
 	 */
 	public void addToTable(Player p){
+		Integer key = Integer.parseInt(p.bibNumber);//adds player to map
+		if(map.containsKey(key)){
+			map.remove(key);
+		}
+		map.put(key, p);
 		
 	}
 	
 	/**
-	 * makes the list
+	 * goes through the map and sort the Players by their time
+	 * and returns a string in with HTML table formatting;
+	 * otherwise if map was empty it returns empty String
 	 */
-	public void getListAsHTML(){
+	public static String getListAsHTML(){
 		
+		String ret = "";
+		List<Player> mainList = new ArrayList<>();
+		for(Player p: map.values()){
+			mainList.add(p);
+		}
+		if(mainList.isEmpty()){
+			ret  = "";
+		}
+		else{
+			List<Player> sorted = sort(mainList);
+			for(int i=0; i<sorted.size(); i++){
+				//ret = ret + sorted.get(i) + "\n";
+			}
+			for(Player p: sorted){
+				ret +=("<tr><td>" + p.firstInitial + "</td>"
+				+ "<td>"+ p.lastName + "</td>" 
+				+ "<td>"+ p.bibNumber + "</td>" 
+				+"<td>"+ p.time + "</td></tr>");
+			}
+		}
+		return ret;
 	}
 	
+	
+	@SuppressWarnings("unchecked")
+	public static List<Player> sort(List<Player> mainList){
+		List<Player>sorted= new ArrayList<>();
+		for(int i =0; i<mainList.size(); i++){
+			sorted.add(mainList.get(i));
+		}
+		sorted.sort(c);
+		return sorted;
+	}
+	
+	public static Comparator<Player> c = new Comparator<Player>(){
+		public int compare(Player p1,Player p2){
+			return p1.time.compareTo(p2.time);
+			
+		}
+	};
 	
 	
 }
